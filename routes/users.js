e4abb19d404eff0566b45339af5ef774211066c7
EmmanuelBeja <emmcodes@gmail.com>
2016-08-01 12:59:27 +0300

    edits

--- routes/users.js
+++ routes/users.js
@@ -7,26 +7,23 @@ var User = require('../models/user');
 
 
 //login
-router.get('/login', function(req,res){
+router.get('/login',function(req,res){
   res.render('login');
 });
 
 //profile page
-router.get('/index', function(req, res) {
-    res.render('index');
+router.get('/profile', function(req, res) {
+    res.render('profile');
 });
-
-
 //get Register
 router.get('/register', function(req,res){
   var errors = "";
   var utaken = "";
   res.render('register', {
-     errors : errors,
-     utaken : utaken,
+    errors : errors,
+    utaken : utaken,
    });
 });
-
 
 //Register user
 router.post('/register', function(req, res){
@@ -51,48 +48,50 @@ router.post('/register', function(req, res){
   req.checkBody('password2','Passwords do not match').equals(req.body.password);
 
   var errors = req.validationErrors();
-      if (errors){
-        var msg = errors.msg;
-        var utaken = "";
-        res.render('register', {
-          errors : errors,
-          msg : msg,
-          utaken : utaken,
-      });
-      console.log(errors);
-      }
-      else {
-        User.getUserByUsername(username, function(err, user){
-          if(err) throw err;
-          if(user){
-            var utaken = "Username taken";
-            var errors = "";
-            var msg = "";
-            res.render('register', {
-              errors : errors,
-              msg:msg,
-              utaken : utaken,
-          });
-          }else {
-             console.log('You have no register errors');
-             User.find
-             var newUser=new User({
-               fname: fname,
-               lname: lname,
-               username: username,
-               email: email,
-               phonenumber: phonenumber,
-             });
-             User.createUser(newUser,function(err, user){
-               if (err) throw err;
-               console.log(user);
-             });
-             req.flash('success_msg', 'you are registered and now can login');
-             res.redirect('/users/login');
-          }
-          });
-      };
+
+if (errors){
+  var msg = errors.msg;
+  var utaken = "";
+  res.render('register', {
+    errors : errors,
+    msg : msg,
+    utaken : utaken
+});
+console.log(errors);
+}
+else {
+  User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(user){
+    var errors = "";
+    var msg = "";
+    var utaken = "Ooops! Username taken."
+    res.render('register', {
+      errors : errors,
+      msg : msg,
+      utaken : utaken
+  });
+    } else{
+    console.log('You have no register errors');
+    var newUser=new User({
+      fname: fname,
+      lname: lname,
+      username: username,
+      email: email,
+      phonenumber: phonenumber,
+      password:password
     });
+    User.createUser(newUser,function(err, user){
+      if (err) throw err;
+      console.log(user);
+    });
+    req.flash('success_msg', 'you are registered and now can login');
+  res.redirect('/users/login');
+  }
+});
+
+  };
+});
 
 
 //pasport local
@@ -134,7 +133,11 @@ router.post('/login',
 
   router.get('/logout', function(req, res){
     req.logout();
+
     req.flash('success_msg', 'You are logged out');
+
     res.redirect('/users/login');
   });
-module.exports = router;
+
+
+module.exports=router;
